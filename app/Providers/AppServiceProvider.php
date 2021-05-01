1f5b14bf42721da0631474e235f113ad15c7296f
baihaki1802 <mbaihakiabdulaziz9a@gmail.com>
2021-05-01 18:14:05 +0700

    tugas besar eki

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,78 @@
+<?php
+namespace App\Providers;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Contracts\Events\Dispatcher;
+use JeroenNoten\LaravelAdminLte\Events\BuildingMenu;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Schema;
+
+class AppServiceProvider extends ServiceProvider
+{
+
+ public function register()
+ {
+ //
+ }
+ public function boot(Dispatcher $events)
+ {
+    Schema::defaultStringLength(191);
+    $events->listen(BuildingMenu::class, function (BuildingMenu $event) {
+    $roles_id = Auth::user()->roles->name;
+    $event->menu->add('MENU');
+    if ($roles_id=='Admin') {
+        $event->menu->add(
+            [
+                    'text'        => 'Dashboard',
+                    'url'         => '/',
+                    'icon'        => 'fas fa-fw fa-home',
+                   
+                ],
+            [
+                'text' => 'User',
+                'url' => 'user',
+                'icon' => 'fas fa-fw fa-user-tie'
+            ],
+            [
+                'text' => 'Pengelolaan Barang',
+                'url' => 'product',
+                'icon' => 'fas fa-fw fa-archive'
+            ],
+            [
+                'text' => 'Kategori Barang',
+                'url' => 'categorie',
+                'icon' => 'fas fa-fw fa-folder'
+            ],
+            [
+                'text' => 'Merek Barang',
+                'url' => 'brand',
+                'icon' => 'fas fa-fw fa-suitcase'
+            ],
+            
+
+            [
+                'text' => 'Transaksi',
+                'url' => '#',
+                'icon' => 'fas fa-fw fa-cash-register'
+            ],
+            [
+                'text'    => 'Laporan',
+                'icon'    => 'fas fa-fw fa-book',
+                'submenu' => [
+                    [
+                        'text' => 'Laporan Barang Masuk',
+                        'url'  => 'laporan',
+                    ],
+                    [
+                        'text'    => 'Laporan Barang Keluar',
+                        'url'     => 'laporan',
+                    ],
+                ],
+            ],
+        );
+    }
+ 
+  
+ 
+});
+}   
+} 
